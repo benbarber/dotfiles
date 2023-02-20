37efddc348477c25d5590e307ec17dec8f49f319
Ben Barber <contact@benbarber.co.uk>
2023-02-20 20:37:11 +0000

    Cleaner ZSH prompt style

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -13,48 +13,56 @@ git_branch() {
   echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
-git_dirty() {
+git_refs () {
+ ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+ echo "${ref#refs/heads/}"
+}
+
+git_status() {
   if $(! $git status -s &> /dev/null)
   then
     echo ""
   else
-    if [[ $($git status --porcelain) == "" ]]
+    if [[ $($git status --porcelain --untracked no) == "" ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+      echo "%{$fg_bold[green]%}$(git_refs)%{$reset_color%}"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      echo "%{$fg_bold[red]%}$(git_refs)%{$reset_color%}"
     fi
   fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
 }
 
 # This assumes that you always have an origin named `origin`, and that you only
 # care about one specific origin. If this is not the case, you might want to use
 # `$git cherry -v @{upstream}` instead.
-need_push () {
+git_commits_ahead () {
   if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
   then
     number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
 
     if [[ $number == 0 ]]
     then
-      echo " "
+      echo ""
     else
-      echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
+      echo " +%{$fg_bold[magenta]%}$number%{$reset_color%}"
     fi
   fi
 }
 
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+git_prompt() {
+  if $(! $git status -s &> /dev/null)
+  then 
+    echo ""
+  else
+    echo "%{$fg_bold[white]%}(%{$reset_color%}$(git_status)$(git_commits_ahead)%{$fg_bold[white]%})%{$reset_color%}"
+  fi
 }
 
-battery_status() {
+directory_prompt() {
+  echo "%{$fg_bold[white]%}%~%{$reset_color%}"
+}
+
+battery_status_prompt() {
   if test ! "$(uname)" = "Darwin"
   then
     exit 0
@@ -66,9 +74,10 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(directory_prompt) $(git_prompt)\n› '
+
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+  export RPROMPT="%{$fg_bold[white]%}%{$reset_color%}"
 }
 
 precmd() {
